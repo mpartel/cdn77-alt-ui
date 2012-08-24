c03762d6ec9ae7b8748d7da2ba86065ee09f074c
Martin Pärtel <martin.partel@gmail.com>
2012-08-24 16:18:17 +0300

    Added rudimentary navigation links.

--- index.php
+++ index.php
@@ -41,6 +41,7 @@ $stats = array(
 if (php_sapi_name() == 'cli') {
     if (isset($failure)) {
         echo $failure;
+        echo "\n";
     } else {
         require "lib/$module.txt.php";
     }
@@ -53,12 +54,49 @@ if (php_sapi_name() == 'cli') {
 <head>
     <meta charset="utf-8" />
     <title>CDN77 stats</title>
+    <style>
+        #nav {
+            margin-bottom: 2em;
+            margin-left: 3em;
+            margin-right: 3em;
+        }
+        #nav a {
+            padding-left: 0.5em;
+            padding-right: 0.5em;
+        }
+        #nav a {
+            color: blue;
+            text-decoration: none;
+        }
+        #nav a:visited {
+            color: blue;
+        }
+        #nav a.current {
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
-    <?php if (isset($failure)): ?>
-        <p class="error"><?php echo htmlspecialchars($failure); ?></p>
-    <?php else: ?>
-        <?php require "lib/$module.html.php"; ?>
-    <?php endif; ?>
+    <section id="nav">
+        <?php
+        $links = array(
+            'stats' => 'Stats',
+            'purge' => 'Purge'
+        );
+        $linksHtml = array();
+        foreach ($links as $key => $title) {
+            $class = ($key == $module) ? 'navlink current' : 'navlink';
+            $linksHtml[] = '<a href="?action=' . $key . '" class="' . $class . '">' . $title . '</a>';
+        }
+        echo join($linksHtml, " | \n");
+        ?>
+    </section>
+    <section id="main">
+        <?php if (isset($failure)): ?>
+            <p class="error"><?php echo htmlspecialchars($failure); ?></p>
+        <?php else: ?>
+            <?php require "lib/$module.html.php"; ?>
+        <?php endif; ?>
+    <section>
 </body>
 </html>
