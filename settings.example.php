16af4b8f91ebef7661d732bbfa154989aab906fd
Martin Pärtel <martin.partel@gmail.com>
2012-08-24 15:55:30 +0300

    .gitignore /testdata, add it as default data path in example settings.

--- settings.example.php
+++ settings.example.php
@@ -3,7 +3,7 @@
 return array(
     'login' => 'user@example.com',
     'cdn_id' => 1234,
-    'api_key_file' => '/path/to/cdn77/key',
-    'history_file' => '/path/to/cdn77/db/history.sqlite3',
+    'api_key_file' => dirname(__FILE__) . '/testdata/key',
+    'history_file' => dirname(__FILE__) . '/testdata/db/history.sqlite3',
     'min_time_between_snapshots' => 10*60 // 10 minutes
 );
